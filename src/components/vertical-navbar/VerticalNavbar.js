ed5a4da43f50c46d09b856d78fc1549f5233c3e9
Alexis <pingrisalexis@gmail.com>
2022-03-14 17:56:54 +0100

    Create Vertical Navbar

---
+++ src/components/vertical-navbar/VerticalNavbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './VerticalNavbar.module.scss'
+import Meditation from '../../assets/meditation.svg'
+import Swim from '../../assets/swim.svg'
+import Bike from '../../assets/bike.svg'
+import Alter from '../../assets/alter.svg'
+
+const VerticalNavbar = () => {
+   return   <div className={styles.vertical_navbar_container}>
+
+                <ul>
+                    <li>
+                        <img src={Meditation} alt={"meditation logo"}/>
+                    </li><li>
+                        <img src={Swim} alt={"swimming logo"}/>
+                    </li><li>
+                        <img src={Bike} alt={"biker logo"}/>
+                    </li><li>
+                        <img src={Alter} alt={"alter logo"}/>
+                    </li>
+                </ul>
+
+                <div>
+                    <span>
+                        Copyright, SportSee 2020
+                    </span>
+                </div>
+
+            </div>
+}
+
+export default VerticalNavbar
