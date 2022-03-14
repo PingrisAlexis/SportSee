77bc840020eabd24c4379e724362a5a733bd813a
Alexis <pingrisalexis@gmail.com>
2022-03-14 18:00:09 +0100

    Component routing logic

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as  Header } from './header/Header';
+export { default as VerticalNavbar } from './vertical-navbar/VerticalNavbar';
+export { default as DailyActivities } from './daily-activities/DailyActivities';
+export { default as AverageSessionDuration } from './average-session-duration/AverageSessionDuration';
+export { default as OverallPerformances } from './overall-performances/OverallPerformances';
+export { default as ObjectiveScore } from './objective-score/ObjectiveScore';
+export { default as DailyKeyCard } from './daily-key-card/DailyKeyCard';
