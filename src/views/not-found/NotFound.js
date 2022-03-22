bfd652dcaea6c3b94916e3a70ae25177c8ace149
Alexis <pingrisalexis@gmail.com>
2022-03-22 11:36:18 +0100

    Upgrade README

--- src/views/not-found/NotFound.js
+++ src/views/not-found/NotFound.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import styles from "./NotFound.module.scss";
-import {NavLink} from "react-router-dom";
+import styles from './NotFound.module.scss';
+import {NavLink} from 'react-router-dom';
 
 export default class NotFound extends Component {
     render() {
