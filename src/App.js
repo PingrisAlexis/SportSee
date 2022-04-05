f6b0cc0912c3aa7de295c1788f8ee0a2a69c5e62
Alexis <pingrisalexis@gmail.com>
2022-04-05 15:23:05 +0200

    Netlify :  freeze path user

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
                 <Header />
                 <VerticalNavbar />
                 <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/users/12" />
-                    </Route>
-                    <Route exact path="/users/:id"  component={UserProfil}/>
+                    {/*<Route path="/" exact>*/}
+                    {/*    <Redirect to="/users/12" />*/}
+                    {/*</Route>*/}
+                    <Route exact path="/users/12"  component={UserProfil}/>
                     <Route component={NotFound}/>
                     <Route path="*">
                         <Redirect to="/not-found" />
